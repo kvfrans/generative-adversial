a1dafbc283f960ce58ad8e20fac184c50c053474
Kevin Frans <kevin@bobthechicken.com>
2016-06-18 11:08:01 -0700

    dcgan testing

---
+++ dcgan/model.py
@@ -0,0 +1,225 @@
+import os
+import time
+from glob import glob
+import tensorflow as tf
+from random import randint
+
+
+from ops import *
+from utils import *
+
+class DCGAN(object):
+    def __init__(self, sess, image_size=108, is_crop=True,
+                 batch_size=64, sample_size = 64, image_shape=[64, 64, 3],
+                 y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
+                 gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
+                 checkpoint_dir=None):
+        """
+
+        Args:
+            sess: TensorFlow session
+            batch_size: The size of batch. Should be specified before training.
+            y_dim: (optional) Dimension of dim for y. [None]
+            z_dim: (optional) Dimension of dim for Z. [100]
+            gf_dim: (optional) Dimension of gen filters in first conv layer. [64]
+            df_dim: (optional) Dimension of discrim filters in first conv layer. [64]
+            gfc_dim: (optional) Dimension of gen untis for for fully connected layer. [1024]
+            dfc_dim: (optional) Dimension of discrim units for fully connected layer. [1024]
+            c_dim: (optional) Dimension of image color. [3]
+        """
+        self.sess = sess
+        self.is_crop = is_crop
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.sample_size = sample_size
+        self.image_shape = image_shape
+
+        self.y_dim = y_dim
+        self.z_dim = z_dim
+
+        self.gf_dim = gf_dim
+        self.df_dim = df_dim
+
+        self.gfc_dim = gfc_dim
+        self.dfc_dim = dfc_dim
+
+        self.c_dim = 3
+
+        # batch normalization : deals with poor initialization helps gradient flow
+        self.d_bn1 = batch_norm(name='d_bn1')
+        self.d_bn2 = batch_norm(name='d_bn2')
+
+        if not self.y_dim:
+            self.d_bn3 = batch_norm(name='d_bn3')
+
+        self.g_bn0 = batch_norm(name='g_bn0')
+        self.g_bn1 = batch_norm(name='g_bn1')
+        self.g_bn2 = batch_norm(name='g_bn2')
+
+        if not self.y_dim:
+            self.g_bn3 = batch_norm(name='g_bn3')
+
+        self.dataset_name = dataset_name
+        self.checkpoint_dir = checkpoint_dir
+        self.build_model()
+
+    def build_model(self):
+
+        self.images = tf.placeholder(tf.float32, [self.batch_size, 32, 32, 3], name='real_images')
+        self.sample_images= tf.placeholder(tf.float32, [self.sample_size, 32, 32, 3], name='sample_images')
+        self.z = tf.placeholder(tf.float32, [None, 100], name='z')
+
+        self.G = self.generator(self.z)
+        self.D, self.D_logits = self.discriminator(self.images)
+
+        self.D_, self.D_logits_ = self.discriminator(self.G, reuse=True)
+
+        self.d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
+        self.d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
+        self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
+
+        self.d_loss = self.d_loss_real + self.d_loss_fake
+
+        t_vars = tf.trainable_variables()
+
+        self.d_vars = [var for var in t_vars if 'd_' in var.name]
+        self.g_vars = [var for var in t_vars if 'g_' in var.name]
+
+        self.saver = tf.train.Saver()
+
+    def train(self, config):
+        """Train DCGAN"""
+        data = glob(os.path.join("./data", config.dataset, "*.jpg"))
+        #np.random.shuffle(data)
+
+        d_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1) \
+                          .minimize(self.d_loss, var_list=self.d_vars)
+        g_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1) \
+                          .minimize(self.g_loss, var_list=self.g_vars)
+        tf.initialize_all_variables().run()
+
+        self.saver = tf.train.Saver()
+
+        counter = 1
+        start_time = time.time()
+
+        if self.load(self.checkpoint_dir):
+            print(" [*] Load SUCCESS")
+        else:
+            print(" [!] Load failed...")
+
+        batch = unpickle("cifar-10-batches-py/data_batch_1")
+
+        for epoch in xrange(10):
+            for idx in xrange(0, 3000):
+                randomint = randint(0,10000 - config.batch_size - 1)
+                trainingData = batch["data"][randomint:config.batch_size+randomint]
+                trainingData = trainingData/255.0
+                trainingData = np.reshape(trainingData,(config.batch_size,32,32,3))
+
+                batch_z = np.random.uniform(-1, 1, [config.batch_size, 100]).astype(np.float32)
+
+                # Update D network
+                self.sess.run([d_optim],feed_dict={ self.images: trainingData, self.z: batch_z })
+
+                # Update G network
+                self.sess.run([g_optim],feed_dict={ self.z: batch_z })
+
+                # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
+                self.sess.run([g_optim],feed_dict={ self.z: batch_z })
+
+                errD_fake = self.d_loss_fake.eval({self.z: batch_z})
+                errD_real = self.d_loss_real.eval({self.images: trainingData})
+                errG = self.g_loss.eval({self.z: batch_z})
+
+                counter += 1
+                print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
+                    % (epoch, idx, 3000,
+                        time.time() - start_time, errD_fake+errD_real, errG))
+
+                if np.mod(counter, 30) == 1:
+                    samples, d_loss, g_loss = self.sess.run(
+                        [self.sampler, self.d_loss, self.g_loss],
+                        feed_dict={self.z: sample_z, self.images: sample_images}
+                    )
+                    save_images(samples, [8, 8],
+                                './samples/train_%s_%s.png' % (epoch, idx))
+                    print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss))
+
+                if np.mod(counter, 500) == 2:
+                    self.save(config.checkpoint_dir, counter)
+
+    def discriminator(self, image, reuse=False, y=None):
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+
+        h0 = lrelu(conv2d(image, self.df_dim, name='d_h0_conv'))
+        h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim*2, name='d_h1_conv')))
+        h2 = lrelu(self.d_bn2(conv2d(h1, self.df_dim*4, name='d_h2_conv')))
+        h3 = linear(tf.reshape(h2, [self.batch_size, -1]), 1, 'd_h3_lin')
+
+        return tf.nn.sigmoid(h3), h3
+
+    def generator(self, z, y=None):
+        # project `z` and reshape
+        self.z_, self.h0_w, self.h0_b = linear(z, self.gf_dim*8*4*4, 'g_h0_lin', with_w=True)
+
+        self.h0 = tf.reshape(self.z_, [-1, 4, 4, self.gf_dim * 8])
+        h0 = tf.nn.relu(self.g_bn0(self.h0))
+
+        self.h1, self.h1_w, self.h1_b = deconv2d(h0,
+            [self.batch_size, 8, 8, self.gf_dim*4], name='g_h1', with_w=True)
+        h1 = tf.nn.relu(self.g_bn1(self.h1))
+
+        h2, self.h2_w, self.h2_b = deconv2d(h1,
+            [self.batch_size, 16, 16, self.gf_dim*2], name='g_h2', with_w=True)
+        h2 = tf.nn.relu(self.g_bn2(h2))
+
+        h3, self.h3_w, self.h3_b = deconv2d(h2,
+            [self.batch_size, 32, 32, 3], name='g_h3', with_w=True)
+
+        return tf.nn.tanh(h3)
+
+    def sampler(self, z, y=None):
+        tf.get_variable_scope().reuse_variables()
+
+        # project `z` and reshape
+        h0 = tf.reshape(linear(z, self.gf_dim*8*4*4, 'g_h0_lin'),
+                        [-1, 4, 4, self.gf_dim * 8])
+        h0 = tf.nn.relu(self.g_bn0(h0, train=False))
+
+        h1 = deconv2d(h0, [self.batch_size, 8, 8, self.gf_dim*4], name='g_h1')
+        h1 = tf.nn.relu(self.g_bn1(h1, train=False))
+
+        h2 = deconv2d(h1, [self.batch_size, 16, 16, self.gf_dim*2], name='g_h2')
+        h2 = tf.nn.relu(self.g_bn2(h2, train=False))
+
+        h3 = deconv2d(h2, [self.batch_size, 32, 32, self.gf_dim*1], name='g_h3')
+
+        return tf.nn.tanh(h3)
+
+    def save(self, checkpoint_dir, step):
+        model_name = "DCGAN.model"
+        model_dir = "%s_%s" % (self.dataset_name, self.batch_size)
+        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.saver.save(self.sess,
+                        os.path.join(checkpoint_dir, model_name),
+                        global_step=step)
+
+    def load(self, checkpoint_dir):
+        print(" [*] Reading checkpoints...")
+
+        model_dir = "%s_%s" % (self.dataset_name, self.batch_size)
+        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
+            return True
+        else:
+            return False
