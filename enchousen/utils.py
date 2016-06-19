e668acbe8d2cf782d5dddaf982d105352b50e96e
Kevin Frans <kevin@bobthechicken.com>
2016-06-18 20:23:22 -0700

    rewrite of code

---
+++ enchousen/utils.py
@@ -0,0 +1,24 @@
+import scipy.misc
+import numpy as np
+import random
+import tensorflow as tf
+
+def get_image(image_path, image_size, is_crop=True):
+    return transform(imread(image_path), image_size, is_crop)
+
+def transform(image, npx=64, is_crop=True):
+    # npx : # of pixels width/height of image
+    if is_crop:
+        cropped_image = center_crop(image, npx)
+    else:
+        cropped_image = image
+    return np.array(cropped_image)/127.5 - 1.
+
+def center_crop(x, crop_h, crop_w=None, resize_w=64):
+    if crop_w is None:
+        crop_w = crop_h
+    h, w = x.shape[:2]
+    j = int(round((h - crop_h)/2.))
+    i = int(round((w - crop_w)/2.))
+    return scipy.misc.imresize(x[j:j+crop_h, i:i+crop_w],
+                               [resize_w, resize_w])
